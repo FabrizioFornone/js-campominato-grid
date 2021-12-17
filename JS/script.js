4787bc7290781f94f7b5a4172236ffd2c7921e11
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-17 16:21:33 +0100

    Main structure created

---
+++ JS/script.js
@@ -0,0 +1,89 @@
+/* L’utente indica un livello di difficoltà (3 pulsanti)
+ in base al quale viene generata una griglia di gioco quadrata,
+  in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.*/
+
+// Funzione di generazione quadrati easy, "container" sarà il mio stampHtml
+
+function easyBoxGenerator(container) {
+  // creo un elemento div dal JS
+  const newBox = document.createElement("div");
+  // Do a lui la classe square che ho nel css
+  newBox.className = "square";
+  // append al container
+  container.append(newBox);
+  container.classList.add("width-500");
+  newBox.addEventListener("click", function () {
+    console.log(this);
+    this.classList.toggle("azure");
+  });
+}
+
+// Funzione di generazione quadrati medium, "container" sarà il mio stampHtml
+
+function mediumBoxGenerator(container) {
+  // creo un elemento div dal JS
+  const newBox = document.createElement("div");
+  // Do a lui la classe square che ho nel css
+  newBox.className = "square";
+  // append al container
+  container.append(newBox);
+  container.classList.add("width-450");
+  newBox.addEventListener("click", function () {
+    console.log(this);
+    this.classList.toggle("azure");
+  });
+}
+
+// Funzione di generazione quadrati medium, "container" sarà il mio stampHtml
+
+function hardBoxGenerator(container) {
+  // creo un elemento div dal JS
+  const newBox = document.createElement("div");
+  // Do a lui la classe square che ho nel css
+  newBox.className = "square";
+  // append al container
+  container.append(newBox);
+  container.classList.add("width-350");
+  newBox.addEventListener("click", function () {
+    console.log(this);
+    this.classList.toggle("azure");
+  });
+}
+
+// dichiarazione costanti associate ad elementi del DOM
+
+const stampHtml = document.querySelector(".hook-js");
+
+const button1 = document.getElementById("js-btn-hook1");
+
+const button2 = document.getElementById("js-btn-hook2");
+
+const button3 = document.getElementById("js-btn-hook3");
+
+// button1 genera ciclo for per creare griglia facile
+
+button1.addEventListener("click", function () {
+  for (i = 1; i <= 100; i++) {
+    easyBoxGenerator(stampHtml);
+  }
+});
+
+// button2 genera ciclo for per creare griglia facile
+
+button2.addEventListener("click", function () {
+  for (i = 1; i <= 81; i++) {
+    mediumBoxGenerator(stampHtml);
+  }
+});
+
+// button1 genera ciclo for per creare griglia facile
+
+button3.addEventListener("click", function () {
+  for (i = 1; i <= 49; i++) {
+    hardBoxGenerator(stampHtml);
+  }
+});
